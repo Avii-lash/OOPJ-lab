e599790aadd0667bf4e783b03885d6622b2c30fe
Avii-lash <avilash.mohanty.bdk@gmail.com>
2025-12-12 13:21:16 +0530

    JAVA LAB_1 qns

---
+++ .idea/LAB_1/q1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class q1{
+    public static void main(String[] args) {
+
+        String name = "Avilash Mohanty";
+        String rollNo = "24155980";
+        String section = "CSE-33";
+        String branch = "CSE(AI&ML)";
+
+        System.out.println("Name: " + name);
+        System.out.println("Roll No: " + rollNo);
+        System.out.println("Section: " + section);
+        System.out.println("Branch: " + branch);
+    }
+}
